b340cc681ee1a936baff28e842014942e6d0e242
ydgin5 <ydginnuch5@gmail.com>
2024-02-20 14:16:04 +0300

    new commit

--- style.js
+++ style.js
@@ -1,10 +1,17 @@
 "use strict";
 document.addEventListener("click", documentActions);
 function documentActions(e) {
-    const  targetElement = e.target;
-   if(targetElement.closest('[data-goto]')) {
-const goto = targetElement.closest('[data-goto]');
-
-
-   }
+  const targetElement = e.target;
+  if (targetElement.closest("[data-goto]")) {
+    const goto = targetElement.closest("[data-goto]");
+    const gotoElement = document.querySelector(goto);
+    const headerHeight = document.querySelector(".header").offsetHeight;
+    if (gotoElement) {
+        window.scrollTo({
+            top:gotoElement.offsetTop - (headerHeight + 15),
+            behavior:"smooth"
+        });
+    }
+    e.preventDefault();
+  }
 }
