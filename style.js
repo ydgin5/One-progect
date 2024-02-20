6e17eeb3a086b68de6a3569219e184ac4131669f
ydgin5 <ydginnuch5@gmail.com>
2024-02-20 14:59:13 +0300

    new commit

--- style.js
+++ style.js
@@ -1,6 +1,9 @@
 "use strict";
 document.addEventListener("click", documentActions);
 function documentActions(e) {
+  if (targetElement.closest(".ico")){
+
+  }
   const targetElement = e.target;
   if (targetElement.closest("[data-goto]")) {
     const goto = targetElement.closest("[data-goto]");
